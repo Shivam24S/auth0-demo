db183b4fcda9640a435b32253ea25ecc3d9e3cd3
Shivam24S <shivamshrivastav8055@gmail.com>
2024-05-03 19:21:14 +0530

    auth0 log in and log out demo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, user, isLoading } =
+    useAuth0();
 
   return (
     <>
-      <h1>hello</h1>
+      {isLoading ?? <h4>Loading...</h4>}
+      <h1>hello </h1> {isAuthenticated ? user.name : null}
       <br />
-      <button
-        onClick={() => {
-          loginWithRedirect();
-        }}
-      >
-        Login
-      </button>
+      {isAuthenticated ? (
+        <button
+          onClick={() => {
+            logout({ logoutParams: { returnTo: window.location.origin } });
+          }}
+        >
+          Log Out
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            loginWithRedirect();
+          }}
+        >
+          Login
+        </button>
+      )}
     </>
   );
 };
