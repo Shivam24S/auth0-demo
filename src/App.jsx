469cc1be875711d18278b9b374f8a85ef753b058
Shivam24S <shivamshrivastav8055@gmail.com>
2024-05-03 18:54:10 +0530

    login btn and auth0 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 const App = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <>
       <h1>hello</h1>
+      <br />
+      <button
+        onClick={() => {
+          loginWithRedirect();
+        }}
+      >
+        Login
+      </button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-uatgf1yu0mx42w76.us.auth0.com"
+    clientId="SvRh0XHK6A9DBEfwBPWT4M0lBwkSoKOw"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
+);
